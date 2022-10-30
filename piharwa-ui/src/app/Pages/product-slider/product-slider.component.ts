9cef9c01389a90fdfada98016d220db834f1f6ad
Dharmpal123 <dharmpal.sing@lmsin.com>
2022-10-30 10:49:05 +0530

    slider images update done

--- piharwa-ui/src/app/Pages/product-slider/product-slider.component.ts
+++ piharwa-ui/src/app/Pages/product-slider/product-slider.component.ts
@@ -1,37 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-product-slider',
-  templateUrl: './product-slider.component.html',
-  styleUrls: ['./product-slider.component.scss']
+    selector: 'app-product-slider',
+    templateUrl: './product-slider.component.html',
+    styleUrls: ['./product-slider.component.scss']
 })
 export class ProductSliderComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
-  imageObject = [{
-    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/5.jpg',
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/5.jpg',
-    title: 'Hummingbirds are amazing creatures'
-}, {
-    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/9.jpg',
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/9.jpg'
-}, {
-    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/4.jpg',
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/4.jpg',
-    title: 'Example with title.'
-},{
-    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/7.jpg',
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/7.jpg',
-    title: 'Hummingbirds are amazing creatures'
-}, {
-    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/1.jpg',
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/1.jpg'
-}, {
-    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/2.jpg',
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/2.jpg',
-    title: 'Example two with title.'
-}];
+    ngOnInit(): void {
+    }
+    imageObject = [{
+        image: '../../../assets/img/gal1.png',
+        thumbImage: '../../../assets/img/gal1.png',
+        title: 'Product Slider'
+    }, {
+        image: '../../../assets/img/gal2.png',
+        thumbImage: '../../../assets/img/gal2.png',
+        title: 'Product Slider'
+    }, {
+        image: '../../../assets/img/gal3.png',
+        thumbImage: '../../../assets/img/gal3.png',
+        title: 'Product Slider'
+    }, {
+        image: '../../../assets/img/gal4.png',
+        thumbImage: '../../../assets/img/gal4.png',
+        title: 'Product Slider'
+    }, {
+        image: '../../../assets/img/gal5.png',
+        thumbImage: '../../../assets/img/gal5.png',
+        title: 'Product Slider'
+    }, {
+        image: '../../../assets/img/gal6.png',
+        thumbImage: '../../../assets/img/gal6.png',
+        title: 'Product Slider'
+    }];
 }
