eed9566dc3e12e0076da897655678658d0b8a42d
aman9893 <aman9893jain@gmail.com>
2022-11-06 16:17:15 +0530

    add

---
+++ piharwa-ui/src/app/Pages/add-adress-contact/add-adress-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-adress-contact',
+  templateUrl: './add-adress-contact.component.html',
+  styleUrls: ['./add-adress-contact.component.scss']
+})
+export class AddAdressContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
