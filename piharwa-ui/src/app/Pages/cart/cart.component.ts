472d529c9bc32330944ec87cc72d99b26c7f4a52
aman9893 <aman9893jain@gmail.com>
2023-01-16 21:54:19 +0530

    add

--- piharwa-ui/src/app/Pages/cart/cart.component.ts
+++ piharwa-ui/src/app/Pages/cart/cart.component.ts
@@ -1,8 +1,8 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonService } from '../common.service';
+import { PaymentsComponent } from '../payments/payments.component';
 import { CartService } from '../products-page/product-details-page/cart-service/cart.service';
-declare var Razorpay: any;
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -11,7 +11,7 @@ declare var Razorpay: any;
 export class CartComponent implements OnInit {
   cartData: any;
   subtotal: any;
-
+  @ViewChild(PaymentsComponent) patment!: PaymentsComponent;
 
   constructor(public cartService :CartService,public route :Router,public commonservice :CommonService,) { }
 
@@ -21,7 +21,6 @@ export class CartComponent implements OnInit {
   }
   getCartData(){
     this.cartData = this.cartService.getItemData()
-    // console.log(this.cartData);
   }
   removeData(data:any) {
     this.cartService.deleteItem(data);
@@ -37,102 +36,7 @@ export class CartComponent implements OnInit {
     this.cartService.clearData();
   }
   toContinue(){
-  
+    this.commonservice.paynow();
   //  this.route.navigate(["/list-Address"]);
-  this.paymentCreate();
-  this.paynow();
   }
-
-  paymentCreate() {
-   let userdata= {
-      "amount": "10000",
-      "currency": "INR"
-    }
-      this.commonservice.paymentSendApi(userdata).subscribe(
-        (data) => this.saveResponse(data),
-        (err) => console.log(err)
-      );
-  }
-  saveResponse(data:any){
-    console.log(data)
-    this.rozaConfrimation(data.data);
-  }
-
-  rozaConfrimation(data:any) {
-    let userdata={
-      "razorpay_order_id": data.offer_id,
-      "razorpay_payment_id": data.id,
-      "razorpay_signature": ""
-    }
-       this.commonservice.verifyPaymentApi(userdata).subscribe(
-         (data) => this.saveResponseRoza(data),
-         (err) => console.log(err)
-       );
-   }
-   saveResponseRoza(data:any){
-     console.log(data)
-   }
-
-   message:any = "Not yet stared";
-   paymentId = "";
-   error = "";
-   title = 'angular-razorpay-intergration';
-   options = {
-     "key": "rzp_test_4Liw7psG1iJoHw",
-     "amount": "200",
-     "name": "Abhijit Gatade",
-     "description": "Web Development",
-     "image": "https://www.abhijitgatade.com/assets/img/favicon.png",
-     "order_id": "",
-     "handler": function (response: any) {
-       var event = new CustomEvent("payment.success",
-         {
-           detail: response,
-           bubbles: true,
-           cancelable: true
-         }
-       );
-       window.dispatchEvent(event);
-     },
-     "prefill": {
-       "name": "",
-       "email": "",
-       "contact": ""
-     },
-     "notes": {
-       "address": ""
-     },
-     "theme": {
-       "color": "#3399cc"
-     }
-   };
- 
-   paynow() {
-     this.paymentId = '';
-     this.error = '';
-     this.options.amount = "200"; //paise
-     this.options.prefill.name = "Abcd";
-     this.options.prefill.email = "abcd@gmail.com";
-     this.options.prefill.contact = "9999999999";
-     var rzp1 = new Razorpay(this.options);
-     rzp1.open();
-     rzp1.on('payment.failed', function (response: any) {
-       //this.message = "Payment Failed";
-       // Todo - store this information in the server
-       console.log(response.error.code);
-       console.log(response.error.description);
-       console.log(response.error.source);
-       console.log(response.error.step);
-       console.log(response.error.reason);
-       console.log(response.error.metadata.order_id);
-       console.log(response.error.metadata.payment_id);
-       //this.error = response.error.reason;
-     }
-     );
-   }
-   @HostListener('window:payment.success', ['$event'])
-   onPaymentSuccess(event: any): void {
-     this.message = "Success Payment";
-   }
- 
 }
--- piharwa-ui/src/app/Pages/common.service.ts
+++ piharwa-ui/src/app/Pages/common.service.ts
@@ -1,8 +1,10 @@
 import { isPlatformBrowser } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { EventEmitter, Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { EventEmitter, HostListener, Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ApiConstants } from '../api-path/api-config';
+declare var Razorpay: any;
+
 function _window(): any {
   // return the global native browser window object
   return window;
@@ -38,4 +40,67 @@ export class CommonService {
       return _window();
     }
   }
+  message:any = "Not yet stared";
+  paymentId = "";
+  error = "";
+  title = 'piharwa shop';
+  options = {
+    "key": "rzp_test_4Liw7psG1iJoHw",
+    "amount": "200",
+    "name": "Aman jain",
+    "description": "Payment for pirharwa",
+    "image": "https://www.abhijitgatade.com/assets/img/favicon.png",
+    "order_id": "",
+    "handler": function (response: any) {
+      var event = new CustomEvent("payment.success",
+        {
+          detail: response,
+          bubbles: true,
+          cancelable: true
+        }
+      );
+      window.dispatchEvent(event);
+    },
+    "prefill": {
+      "name": "",
+      "email": "",
+      "contact": ""
+    },
+    "notes": {
+      "address": ""
+    },
+    "theme": {
+      "color": "#3399cc"
+    }
+  };
+
+  paynow() {
+    this.paymentId = '';
+    this.error = '';
+    this.options.amount = "20000"; //paise
+    this.options.prefill.name = "Abcd";
+    this.options.prefill.email = "abcd@gmail.com";
+    this.options.prefill.contact = "9999999999";
+    var rzp1 = new Razorpay(this.options);
+    rzp1.open();
+    rzp1.on('payment.failed', function (response: any) {
+      //this.message = "Payment Failed";
+      // Todo - store this information in the server
+      console.log(response.error.code);
+      console.log(response.error.description);
+      console.log(response.error.source);
+      console.log(response.error.step);
+      console.log(response.error.reason);
+      console.log(response.error.metadata.order_id);
+      console.log(response.error.metadata.payment_id);
+      //this.error = response.error.reason;
+    }
+    );
+  }
+  @HostListener('window:payment.success', ['$event'])
+  onPaymentSuccess(event: any): void {
+    this.message = "Success Payment";
+  }
+
+
 }
