079e0a4bfb3c8c8603b807530a05aed46a1fd681
vamit7631 <vamit7631@gmail.com>
2023-01-28 22:24:05 +0530

    account information functionality added

--- piharwa-ui/src/app/Pages/profile-info/profile-info.component.ts
+++ piharwa-ui/src/app/Pages/profile-info/profile-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonService } from '../../Pages/common.service';
 
 @Component({
   selector: 'app-profile-info',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-info.component.scss']
 })
 export class ProfileInfoComponent implements OnInit {
-
-  constructor() { }
+  profileData: any;
+  constructor(public commonService: CommonService, public router: Router) { }
 
   ngOnInit(): void {
+    this.getProfileDetails();
   }
+
+
+  getProfileDetails() {
+    this.commonService.profileApi().subscribe((data) => this.getProfileDetailsApi(data));
+  }
+  
+  getProfileDetailsApi(data:any){
+    if (data.status === true) {
+      this.profileData = data.data
+      console.log(this.profileData,"=====================profileData");
+    }
+
+  }
+
 
 }
