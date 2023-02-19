86082739687177f2e3e468e9dd82219b6340a1a6
vamit7631 <vamit7631@gmail.com>
2023-02-20 01:33:27 +0530

    Added contact form for All users

--- piharwa-ui/src/app/Pages/contact-us/contact-us.component.ts
+++ piharwa-ui/src/app/Pages/contact-us/contact-us.component.ts
@@ -1,15 +1,72 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CommonService } from '../common.service';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  NgForm,
+  Validators,
+} from '@angular/forms';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent implements OnInit {
-
-  constructor() { }
+  contactusForm!: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    public commonservice: CommonService,
+  ) { }
 
   ngOnInit(): void {
+    this.ContactForm()
   }
+
+
+  ErrorMessage(value: any) {
+    return 'Please ' + value;
+  }
+
+  private ContactForm() {
+    this.contactusForm = this.formBuilder.group({
+      firstName: new FormControl('', {
+        validators: [Validators.required, Validators.maxLength(55)],
+        updateOn: 'change',
+      }),
+      lastName: new FormControl('', {
+        validators: [Validators.required, Validators.maxLength(55)],
+        updateOn: 'change',
+      }),
+      subject: new FormControl('', {
+        validators: [Validators.required, Validators.maxLength(55)],
+        updateOn: 'change',
+      }),
+      emailId: new FormControl('', {
+        validators: [Validators.required, Validators.maxLength(55),Validators.email],
+        updateOn: 'change',
+      }),
+      description: new FormControl(''),
+    });
+  }
+
+
+  onSubmitContactUs() {
+    let contactDetails = this.contactusForm.value;
+    this.commonservice.sendMailForContact(contactDetails).subscribe(
+      (data: any) => { console.log(data,"contact data") },
+      (err: any) => console.log(err)
+    );
+    this.contactusForm.reset();
+
+
+
+  }
+
 
 }
